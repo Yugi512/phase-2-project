2b2a636717938de935cfa017c36679fc27c4c6ad
Kelvin Gil <gilkelvin2@gmail.com>
2024-05-15 22:11:44 -0400

    created nl page

---
+++ src/NationalLeague.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { useState,useEffect } from "react";
+
+
+function NationalLeague(){
+    
+
+    return (
+        <div>
+            <h1>National League stats</h1>
+        </div>
+    )
+}
+
+export default NationalLeague;
