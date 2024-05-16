0b1c3cf5595e5c2be44611fdab18bddd6599e53b
Kelvin Gil <gilkelvin2@gmail.com>
2024-05-15 22:10:38 -0400

    created home page

---
+++ src/Home.js
@@ -0,0 +1,15 @@
+import { useState, useEffect } from "react";
+import { Outlet } from "react-router-dom";
+import UserCard from "../components/UserCard";
+
+
+function Home(){
+
+    return (
+        <div>
+            <h1>home</h1>
+        </div>
+    )
+}
+
+export default Home;
