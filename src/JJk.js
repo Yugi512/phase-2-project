6058c22cba051d7a5689d07f873db56cee4e8d54
Kelvin Gil <gilkelvin2@gmail.com>
2024-05-21 18:25:49 -0400

    done with layout

---
+++ src/JJk.js
@@ -0,0 +1,30 @@
+import { useOutletContext, useParams } from "react-router-dom";
+import Form from "./Form";
+
+function JJK(){
+    const { jjk, setJJK, url1 } = useOutletContext() 
+    
+
+    const characterCards = jjk.map((character, index) => <div className="card" id={index} key={index}>
+        <img className="cc" src={character.imgurl} ></img>
+        <h2>{character.name}</h2>
+    </div>
+    )
+
+    function setJjk(character){
+        setJJK([...jjk,character])
+    }
+
+    return (
+        <div >
+            
+            <div> 
+                <h1 className="title"> Jujutsu kaisen Characters </h1>
+                <Form url={url1} setArray={setJjk}/>
+                {characterCards}
+            </div>
+        </div>
+    )
+}
+
+export default JJK
