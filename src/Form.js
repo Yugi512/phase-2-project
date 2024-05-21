79f90109c444de441142d43a97abd3fa3d0aeb7b
Kelvin Gil <gilkelvin2@gmail.com>
2024-05-21 00:38:08 -0400

    character form created

---
+++ src/Form.js
@@ -0,0 +1,11 @@
+import { useState, useEffect } from "react";
+
+function Form({url}){
+    console.log(url)
+
+    return (
+        <div className="form"> Form </div>
+    )
+}
+
+export default Form
