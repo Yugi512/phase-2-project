ce84b25028acbf448ff4a992fdd6ccd237e592ec
Kelvin Gil <gilkelvin2@gmail.com>
2024-05-21 18:23:59 -0400

    completed form

--- src/Form.js
+++ src/Form.js
@@ -1,10 +1,62 @@
 import { useState, useEffect } from "react";
 
-function Form({url}){
+function Form({url, setArray}){
+    const [newCharacter, setCharacter] = useState({
+        name: "",
+        imgurl: ""
+    });
     console.log(url)
 
+    function handleChange(event){
+        setCharacter({
+          ...newCharacter,
+          [event.target.id]: event.target.value
+        })
+    }
+
+    function handleSubmit(e){
+        e.preventDefault()
+        fetch(`${url}`,{
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(newCharacter)
+        })
+        .then((r) => r.json())
+        .then((character) => setArray(character) )
+    }
+    
+
     return (
-        <div className="form"> Form </div>
+        <div className="form"> 
+            <form onSubmit={(e) => handleSubmit(e)}>
+                <input
+                    type="text"
+                    id="name"
+                    value={newCharacter.name}
+                    placeholder="enter characters name"
+                    className="input-text"
+                    onChange={handleChange}
+                />
+                <br />
+                <input
+                    type="text"
+                    id="imgurl"
+                    value={newCharacter.imgurl}
+                    placeholder="enter a img url"
+                    className="input-text"
+                    onChange={handleChange}
+                />
+                <br />
+                <input
+                    type="submit"
+                    name="submit"
+                    value="add a character"
+                    className="submit"
+                />
+            </form>
+        </div>
     )
 }
 
